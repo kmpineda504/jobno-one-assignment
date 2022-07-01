c161152b314e19f7d63e1a7de65a684d991cb7a8
kmpineda504 <kmpineda504@outlook.com>
2022-07-01 22:30:24 +0200

    working on the todo completed items

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -7,13 +7,26 @@ const Todo = ({ text, todo, todos, setTodos }) => {
     //Events
     const deleteHandler = () =>{
       setTodos(todos.filter((el) => el.id !== todo.id ));
-    }
+    };
+
+    const completeHandler = () =>{
+      setTodos(
+        todos.map((item) => {
+          if(item.id === todo.id){
+            return{
+              ...item, completed: !item.completed
+            };
+          }
+          return item;
+        })
+      )
+    };
 
     return (
       <>
           <li>
               <p className='todo-item'>{text}</p>
-              <button className='btn btn-secondary btn-check'><FaCheck/></button>
+              <button onClick={completeHandler} className='btn btn-secondary btn-check'><FaCheck/></button>
               <button onClick={deleteHandler} className='btn btn-secondary btn-delete'><FaTrash/></button>
           </li>
       </>
