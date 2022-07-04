58b0965be30177c6b4bd3660030c344c86b5dbeb
kmpineda504 <kmpineda504@outlook.com>
2022-07-04 11:06:54 +0200

    save to local

--- src/App.js
+++ src/App.js
@@ -1,12 +1,57 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import Header from "./components/Header";
 import Form from "./components/Form";
 
 function App() {
+
+  const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState ([]);
+  const [status, setStatus] = useState ("all");
+  const [filteredTodos, setfilteredTodos] = useState ([]);
+
+
+  //use effect - just once
+  useEffect(() => { 
+    let todoLocal = window.localStorage.getItem('todos');
+    if ( todoLocal !== null)  setTodos(JSON.parse(todoLocal));
+  }, []);
+
+  useEffect(() => {
+    //save local
+    window.localStorage.setItem('todos', JSON.stringify(filteredTodos));
+  });
+
+  //use effect - on changes in state
+  useEffect(() => {
+    const filterHandler = () => {
+      switch (status) {
+        case 'completed':
+          setfilteredTodos(todos.filter(todo => todo.completed === true))
+          break;
+        case 'uncompleted':
+          setfilteredTodos(todos.filter(todo => todo.completed === false));
+          break;
+        default:
+          setfilteredTodos(todos);
+          break;
+      }
+    }    
+    filterHandler();
+  }, [todos, status]);
+
   return (
     <div className="container">
       <Header /> 
-      <Form />
+      <Form 
+        inputText={inputText}
+        setInputText={setInputText}
+        todos={todos}
+        setTodos={setTodos}
+        status={status}
+        setStatus={setStatus}
+        filteredTodos={filteredTodos}
+        setfilteredTodos={setfilteredTodos}
+      />
     </div>
   );
 }
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,55 +1,17 @@
-import React, {useState, useEffect} from 'react';
+import React from 'react';
 import Input from './Input';
 import Select from './Select';
 import Todolist from './Todolist';
 
 
-const Form = () => {
-    //state stuffs
-    const [inputText, setInputText] = useState("");
-    const [todos, setTodos] = useState ([]);
-    const [status, setStatus] = useState ("all");
-    const [filteredTodos, setfilteredTodos] = useState ([]);
- 
-    //use effect - just once
-    useEffect(() => {
-        const getLocalTodos = () => {
-            if (localStorage.getItem('todos') === null) {
-                localStorage.setItem('todos', JSON.stringify([]));
-            }
-            else {
-                let todoLocal = JSON.parse(localStorage.getItem('todos'));
-                setTodos(todoLocal);
-            }
-        }
-
-        getLocalTodos();
-    }, []);
-
-    //use effect - on changes in state
-    useEffect(() => {
-        //filter todos
-        const filterHandler = () => {
-            switch (status) {
-                case 'completed':
-                    setfilteredTodos(todos.filter(todo => todo.completed === true))
-                    break;
-                case 'uncompleted':
-                    setfilteredTodos(todos.filter(todo => todo.completed === false));
-                    break;
-                default:
-                    setfilteredTodos(todos);
-                    break;
-            }
-        }
-        //save to local
-        const saveLocalTodos = () => {
-            localStorage.setItem('todos', JSON.stringify(todos));
-        }
-
-        filterHandler();
-        saveLocalTodos();
-    }, [todos, status]); 
+const Form = ({inputText,
+    setInputText,
+    todos,
+    setTodos,
+    status,
+    setStatus,
+    filteredTodos,
+    setfilteredTodos}) => {
     
     return (
         <>
